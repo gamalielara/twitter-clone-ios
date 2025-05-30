871f59eb8d422fe1f624b36fbd5a5a7a51eb58cb
gamalielara <aragamalielboanerges@gmail.com>
2025-05-30 09:57:32 +0700

    Initial Commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by Ara Gamaliel on 5/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Twitter CloneTests/Twitter_CloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneTests.swift
+//  Twitter CloneTests
+//
+//  Created by Ara Gamaliel on 5/30/25.
+//
+
+import Testing
+@testable import Twitter_Clone
+
+struct Twitter_CloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
